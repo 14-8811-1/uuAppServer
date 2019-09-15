1208d6e15893030af368915cf073fb8aeda2607b
Vojtech Sebek <vojtech.sebek@plus4u.net>
2019-09-15 21:59:50 +0200

    example 2 - dtoIn, uuUri

---
+++ uu_jokes_maing01-server/app/api/controllers/joke-controller.js
@@ -0,0 +1,16 @@
+"use strict";
+class JokeController {
+  create(ucEnv) {
+    let dtoIn = ucEnv.getDtoIn();
+    let awid = ucEnv.getUri().getAwid();
+    let dtoOut = {
+      awid,
+      joke: dtoIn.joke,
+      uuAppErrorMap: {}
+    };
+
+    return dtoOut;
+  }
+}
+
+module.exports = new JokeController();
