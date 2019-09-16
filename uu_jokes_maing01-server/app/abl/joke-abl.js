7d3b12784a28eca2777295b40d8edb7ceca471b6
Vojtech Sebek <vojtech.sebek@plus4u.net>
2019-09-16 13:58:11 +0200

    example 12 - authentication, authorization

--- uu_jokes_maing01-server/app/abl/joke-abl.js
+++ uu_jokes_maing01-server/app/abl/joke-abl.js
@@ -10,6 +10,9 @@ const WARNINGS = {
   },
   getUnsupportedKeys: {
     code: `${Errors.Get.UC_CODE}unsupportedKeys`
+  },
+  setActiveUnsupportedKeys: {
+    code: `${Errors.SetActive.UC_CODE}unsupportedKeys`
   }
 };
 
@@ -19,7 +22,7 @@ class JokeAbl {
     this.dao = DaoFactory.getDao("joke");
   }
 
-  async create(awid, dtoIn) {
+  async create(awid, dtoIn, authorizationResult, identity) {
     let validationResult = this.validator.validate("jokeCreateDtoInType", dtoIn);
     let uuAppErrorMap = ValidationHelper.processValidationResult(
       dtoIn,
@@ -28,6 +31,14 @@ class JokeAbl {
       Errors.Create.InvalidDtoIn
     );
 
+    if (authorizationResult.getAuthorizedProfiles().includes("Authorities")) {
+      dtoIn.state = "active";
+    } else {
+      dtoIn.state = "pending";
+    }
+
+    dtoIn.author = identity.getName();
+    dtoIn.uuIdentity = identity.getUuIdentity();
     dtoIn.awid = awid;
     let dtoOut;
     try {
@@ -60,7 +71,29 @@ class JokeAbl {
       throw new Errors.Get.JokeDoesNotExist(uuAppErrorMap, { code: dtoIn.code });
     }
 
-    // hds 4
+    joke.uuAppErrorMap = uuAppErrorMap;
+    return joke;
+  }
+
+  async setActive(awid, dtoIn) {
+    let validationResult = this.validator.validate("jokeSetActiveDtoInType", dtoIn);
+    let uuAppErrorMap = ValidationHelper.processValidationResult(
+      dtoIn,
+      validationResult,
+      WARNINGS.setActiveUnsupportedKeys.code,
+      Errors.SetActive.InvalidDtoIn
+    );
+
+    let joke;
+    try {
+      joke = await this.dao.setActive(awid, dtoIn.code);
+    } catch (e) {
+      if (e instanceof ObjectStoreError) {
+        throw new Errors.SetActive.SetStateFailed({ uuAppErrorMap }, e);
+      }
+      throw e;
+    }
+
     joke.uuAppErrorMap = uuAppErrorMap;
     return joke;
   }
